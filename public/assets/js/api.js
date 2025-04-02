a9bcfcc102669edad824e7c1adf90688b47b927e
willianderson <106893704+Willian1661@users.noreply.github.com>
2025-04-02 17:47:01 -0300

    Update api.js
    remove my API key i've forgot  😅

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,6 +1,6 @@
 var muscle = 'biceps'
 fetch('https://api.api-ninjas.com/v1/exercises?muscle=' + muscle, {
-    headers: { 'X-Api-Key': 'd1BDAMa/72jD4C++1s9Uww==ZH7fr5KjYTjBXREG' }
+    headers: { 'X-Api-Key': '' }
 })
 .then(response => response.json())
 .then(data => console.log(data))
